fd4dc2ac380340fccc13e9d842071dfb76d10c0d
oatsoda <andrew+github@milne.tel>
2022-07-19 17:30:12 +0100

    More test data.

--- TeePee.Tests/TestData/NonJsonContentTypesData.cs
+++ TeePee.Tests/TestData/NonJsonContentTypesData.cs
@@ -17,6 +17,18 @@ namespace TeePee.Tests.TestData
                                       };
             yield return new object[] { new StringContent("here's my content", Encoding.UTF8) };
             yield return new object[] { new StringContent("here's my content", Encoding.ASCII) };
+            yield return new object[] { new MultipartFormDataContent
+                                        {
+                                            new StringContent("some string"),
+                                            new ByteArrayContent(new byte[]{ 65, 98, 48 })
+                                        } };
+            yield return new object[] { new FormUrlEncodedContent(
+                                                                  new Dictionary<string, string>
+                                                                  {
+                                                                      { "k1", "v1" },
+                                                                      { "k2", "v2" },
+                                                                  })
+                                      };
         }
     }
 }
