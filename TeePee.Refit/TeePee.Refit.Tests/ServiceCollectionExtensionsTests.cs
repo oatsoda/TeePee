d6b29b44b4d6a5647abcc364c13b23d2db0ad65c
oatsoda <andrew+github@milne.tel>
2026-01-10 12:00:43 +0000

    Replace reflection with normal Refit which chains on to existing setup

--- TeePee.Refit/TeePee.Refit.Tests/ServiceCollectionExtensionsTests.cs
+++ TeePee.Refit/TeePee.Refit.Tests/ServiceCollectionExtensionsTests.cs
@@ -19,29 +19,25 @@ namespace TeePee.Refit.Tests
         {
             // Given
 
-
-            // - Production Code Setup -
+            // ** Production Code Setup
             var services = new ServiceCollection();
             services.AddRefitClient<IApiService>()
                     .ConfigureHttpClient(c => c.BaseAddress = new("https://api.github.com"));
 
-            // - Test Setup -
+            // ** Test Setup
             var builder = new TeePeeBuilder();
             builder.ForRequest("https://api.github.com/users/abc-123", HttpMethod.Get)
                    .Responds()
                    .WithBody(new { Name = "User's Name" })
                    .WithStatus(HttpStatusCode.OK);
 
-
             // When
 
-
-            // - Subject Under Test -
+            // ** Subject Under Test
             services.AttachToRefitInterface<IApiService>(await builder.Build());
 
-            // - Simulate Production Code - 
+            // ** Simulate Production Code
             var user = await services.BuildServiceProvider().GetRequiredService<IApiService>().GetUser("abc-123");
-
 
             // Then
             Assert.Equal("User's Name", user.Name);
