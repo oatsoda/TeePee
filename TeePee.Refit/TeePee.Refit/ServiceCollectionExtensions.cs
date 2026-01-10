74841c5c742fd00fedf8c7d78849ef747b14b4b4
oatsoda <andrew+github@milne.tel>
2026-01-10 12:06:31 +0000

    Replace reflection with normal Refit which chains on to existing setup (#61)

--- TeePee.Refit/TeePee.Refit/ServiceCollectionExtensions.cs
+++ TeePee.Refit/TeePee.Refit/ServiceCollectionExtensions.cs
@@ -1,41 +1,20 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using System.Collections.Concurrent;
-using System.Reflection;
+using Refit;
 
 namespace TeePee.Refit
 {
     public static class ServiceCollectionExtensions
     {
-        private static readonly Lazy<MethodInfo> s_RefitGetHttpClientFactoryNameMethod = new(() =>
-        {
-#pragma warning disable 219 //Force assembly reference to Refit
-            var a = nameof(global::Refit.HttpClientFactoryExtensions.AddRefitClient);
-#pragma warning restore 219
-            var type = Type.GetType("Refit.UniqueName, Refit")!;
-            return type.GetMethods(BindingFlags.Public | BindingFlags.Static).Single(m => m.Name == "ForType" && !m.IsGenericMethod);
-        });
-
-        private static readonly ConcurrentDictionary<Type, string> s_HttpClientFactoryNames = new();
-
-        private static string GetRefitHttpFactoryName<T>()
-        {
-            var type = typeof(T);
-            return s_HttpClientFactoryNames.GetOrAdd(type, t => (string)s_RefitGetHttpClientFactoryNameMethod.Value.Invoke(null, new object[] { t }));
-        }
-
         public static IServiceCollection AttachToRefitInterface<TRefitInterface>(this IServiceCollection serviceCollection, TeePee teePee)
+            where TRefitInterface : class
         {
             // Get Delegating Handler to inject into the Http pipeline
             var requestHandler = teePee.HttpHandler;
             serviceCollection.AddTransient(_ => requestHandler);
 
-            // Reflection: Get the registered HttpClient Name as used in Refit AddRefitClient (internal UniqueName helper class)
-            var httpClientFactoryName = GetRefitHttpFactoryName<TRefitInterface>();
-
-            // Add same-named HttpClient Name into same ServiceCollection which should allow us to append to existing options on that Http Named instance
-            serviceCollection
-                .AddHttpClient(httpClientFactoryName)
-                .AddHttpMessageHandler(_ => requestHandler);
+            serviceCollection.AddRefitClient<TRefitInterface>() // This should continue configuring the same Refit client
+                .AddHttpMessageHandler(_ => requestHandler)
+                ;
 
             return serviceCollection;
         }
